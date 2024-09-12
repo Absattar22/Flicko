2201e5158c3a00f9b66e00c7b522f391a305ece2
zeyadabdelsattar <zeyadabdelsattar@gmail.com>
2024-09-12 04:18:48 +0300

    Add watch later Button

--- lib/widgets/custom_movie_title.dart
+++ lib/widgets/custom_movie_title.dart
@@ -1,9 +1,16 @@
 import 'package:flicko/widgets/custom_elevated_button.dart';
 import 'package:flutter/material.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 
-class CustomMovieTitle extends StatelessWidget {
+class CustomMovieTitle extends StatefulWidget {
   const CustomMovieTitle({super.key});
 
+  @override
+  State<CustomMovieTitle> createState() => _CustomMovieTitleState();
+}
+
+class _CustomMovieTitleState extends State<CustomMovieTitle> {
+  bool isWatchLater = false;
   @override
   Widget build(BuildContext context) {
     return Padding(
@@ -11,13 +18,29 @@ class CustomMovieTitle extends StatelessWidget {
       child: Column(
         crossAxisAlignment: CrossAxisAlignment.start,
         children: [
-          const Text(
-            'Interstellar',
-            style: TextStyle(
-              fontSize: 36,
-              fontWeight: FontWeight.bold,
-              color: Colors.white,
-            ),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: [
+              const Text(
+                'Interstellar',
+                style: TextStyle(
+                  fontSize: 36,
+                  fontWeight: FontWeight.bold,
+                  color: Colors.white,
+                ),
+              ),
+              IconButton(
+                onPressed: () {
+                  setState(() {
+                    isWatchLater = !isWatchLater;
+                  });
+                },
+                icon: FaIcon(FontAwesomeIcons.solidClock,
+                    color: isWatchLater
+                        ? const Color.fromARGB(255, 227, 166, 12)
+                        : Colors.white),
+              ),
+            ],
           ),
           const SizedBox(height: 5),
           Row(
