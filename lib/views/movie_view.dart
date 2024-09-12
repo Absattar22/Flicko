a720b1931a63602b97b6c62617b860199cf20caf
zeyadabdelsattar <zeyadabdelsattar@gmail.com>
2024-09-12 04:21:12 +0300

    organize the movie view

--- lib/views/movie_view.dart
+++ lib/views/movie_view.dart
@@ -1,12 +1,54 @@
-import 'package:flicko/constants.dart';
-import 'package:flicko/widgets/custom_app_bar.dart';
-import 'package:flicko/widgets/custom_now_showing.dart';
+import 'package:flicko/views/categories_view.dart';
+import 'package:flicko/views/home_view.dart';
+import 'package:flicko/views/recommendation_view.dart';
+import 'package:flicko/views/search_view.dart';
+import 'package:flicko/views/watch_list_view.dart';
+import 'package:flicko/widgets/custom_navigation_bar.dart';
 import 'package:flutter/material.dart';
+import 'package:flicko/constants.dart';
 
-class MovieView extends StatelessWidget {
+
+class MovieView extends StatefulWidget {
   const MovieView({super.key});
 
   static const String id = 'movieView';
+
+  @override
+  State<MovieView> createState() => _MovieViewState();
+}
+
+class _MovieViewState extends State<MovieView> {
+  int currentIndex = 0;
+
+  final List<Widget> _screens = [
+    const HomeView(),
+    const WatchListView(),
+    const RecommendationView(),
+    const SearchView(),
+    const CategoriesView(),
+  ];
+
+  final List<IconData> iconList = [
+    Icons.home,
+    Icons.bookmark,
+    Icons.recommend,
+    Icons.search,
+    Icons.category_outlined,
+  ];
+
+  final List<String> labels = [
+    'Home',
+    'Watchlist',
+    'Recommend',
+    'Search',
+    'Categories',
+  ];
+
+  void onTabTapped(int index) {
+    setState(() {
+      currentIndex = index;
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -14,42 +56,16 @@ class MovieView extends StatelessWidget {
 
     return Scaffold(
       backgroundColor: kPrimaryColor,
-      body: CustomScrollView(
-        slivers: [
-          SliverAppBar(
-            elevation: 0,
-            forceElevated: false,
-            pinned: false,
-            floating: true,
-            snap: true,
-            expandedHeight: screenHeight * 0.01,
-            flexibleSpace: const FlexibleSpaceBar(
-              centerTitle: true,
-              background: CustomAppBar(),
-            ),
-          ),
-          SliverList(
-            delegate: SliverChildListDelegate(
-              [
-                const CustomMovieViewBuilder(
-                  title: 'Now Showing',
-                  img: 'assets/images/Interstellar.jpg',
-                  movieTitle: 'Interstellar',
-                ),
-                const CustomMovieViewBuilder(
-                  title: 'Top Rated',
-                  img: 'assets/images/AllTheBrightPlaces.jpg',
-                  movieTitle: 'All The Bright Places',
-                ),
-                const CustomMovieViewBuilder(
-                  title: 'Top Watched',
-                  img: 'assets/images/furious7.jpg',
-                  movieTitle: 'Furious 7',
-                ),
-              ],
-            ),
-          ),
-        ],
+      body: IndexedStack(
+        index:
+            currentIndex, 
+        children: _screens,
+      ),
+      bottomNavigationBar: CustomBottomNavigationBar(
+        currentIndex: currentIndex,
+        onTabTapped: onTabTapped,
+        iconList: iconList,
+        labels: labels,
       ),
     );
   }
