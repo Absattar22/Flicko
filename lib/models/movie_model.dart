ec322d7fdc411c76133c0d1468afa9b988c85b83
zeyadabdelsattar <zeyadabdelsattar@gmail.com>
2024-09-16 20:54:05 +0300

    Implement the movie model

---
+++ lib/models/movie_model.dart
@@ -0,0 +1,29 @@
+// movie.dart
+class Movie {
+  final String title;
+  final String overview;
+  final String posterPath;
+  final String releaseDate;
+  final String language;
+  final double voteAverage;
+
+  Movie({
+    required this.title,
+    required this.overview,
+    required this.posterPath,
+    required this.releaseDate,
+    required this.language,
+    required this.voteAverage,
+  });
+
+  factory Movie.fromJson(Map<String, dynamic> json) {
+    return Movie(
+      title: json['title'],
+      overview: json['overview'],
+      posterPath: json['poster_path'] ?? '', 
+      releaseDate: json['release_date'] ?? 'Unknown', 
+      language: json['original_language'] ?? 'Unknown', 
+      voteAverage: double.parse((json['vote_average'] as num).toString()).toDouble(),
+    );
+  }
+}
