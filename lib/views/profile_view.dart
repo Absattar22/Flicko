a6ec562f59209b24baa57b794a95bd05b6b24331
zeyadabdelsattar <zeyadabdelsattar@gmail.com>
2024-09-16 20:55:49 +0300

    Create profile view

---
+++ lib/views/profile_view.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+class ProfileView extends StatelessWidget {
+  const ProfileView({super.key});
+
+  static const String id = 'profileView';
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
