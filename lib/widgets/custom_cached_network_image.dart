efb533306079bc5d79747023a9ce2683fc0aa3fe
zeyadabdelsattar <zeyadabdelsattar@gmail.com>
2024-09-17 11:07:37 +0300

    Create custom cached network image

---
+++ lib/widgets/custom_cached_network_image.dart
@@ -0,0 +1,21 @@
+import 'package:cached_network_image/cached_network_image.dart';
+import 'package:flicko/constants.dart';
+import 'package:flutter/material.dart';
+
+class CustomCachedNetworkImage extends StatelessWidget {
+  const CustomCachedNetworkImage({super.key , required this.imageUrl});
+
+
+  final String imageUrl;
+
+  @override
+  Widget build(BuildContext context) {
+    return CachedNetworkImage(
+      imageUrl:
+          imageUrl,
+      fit: BoxFit.cover,
+      placeholder: (context, url) =>
+          CircularProgressIndicator(color: kSecondaryColor),
+    );
+  }
+}
