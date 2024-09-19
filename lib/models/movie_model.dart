38c9894ae8d0340270c0ef63ac8fcc8f412e5fe9
zeyadabdelsattar <zeyadabdelsattar@gmail.com>
2024-09-19 21:29:54 +0300

    Add id and genre variables for the movies model

--- lib/models/movie_model.dart
+++ lib/models/movie_model.dart
@@ -1,38 +1,45 @@
 // movie.dart
 class Movie {
+  final int id;
   final String title;
   final String overview;
   final String posterPath;
   final String releaseDate;
   final String language;
-  final double voteAverage;
+  final double rating;
+  final List<int> genre;
+
 
   Movie({
+    required this.genre,
+    required this.id,
     required this.title,
     required this.overview,
     required this.posterPath,
     required this.releaseDate,
     required this.language,
-    required this.voteAverage,
+    required this.rating,
   });
 
   factory Movie.fromJson(Map<String, dynamic> json) {
     return Movie(
+      genre: List<int>.from(json['genre_ids']),
+      id: json['id'],
       title: json['title'],
       overview: json['overview'],
       posterPath: json['poster_path'] ?? '',
       releaseDate: json['release_date'] ?? 'Unknown',
       language: json['original_language'] ?? 'Unknown',
-      voteAverage: double.parse((json['vote_average'] as num).toString()).toDouble(),
+      rating: double.parse((json['vote_average'] as num).toString()).toDouble(),
     );
   }
 
   // Method to return the full image URL
   String fullImageUrl() {
     if (posterPath.isNotEmpty) {
-      return 'https://image.tmdb.org/t/p/w500$posterPath';
+      return 'https://image.tmdb.org/t/p/w400$posterPath';
     } else {
-      return ''; 
+      return '';
     }
   }
 }
