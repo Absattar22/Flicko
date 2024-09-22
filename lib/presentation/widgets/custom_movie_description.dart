19569aee74226df29d75d724bb676b87472d5d17
zeyadabdelsattar <zeyadabdelsattar@gmail.com>
2024-09-23 00:19:40 +0300

    make the design responsive and add the photos of the movie section

--- lib/presentation/widgets/custom_movie_description.dart
+++ lib/presentation/widgets/custom_movie_description.dart
@@ -1,31 +1,47 @@
 import 'package:flutter/material.dart';
 
 class CustomMovieDescription extends StatelessWidget {
-  const CustomMovieDescription({super.key , required this.description } ); 
- final String description;
+  const CustomMovieDescription({super.key, required this.description});
+  final String description;
   @override
   Widget build(BuildContext context) {
-    return  Padding(
-      padding: const EdgeInsets.symmetric(horizontal: 16.0),
+    final screenHeight = MediaQuery.of(context).size.height;
+    final screenWidth = MediaQuery.of(context).size.width;
+    return Padding(
+      padding: EdgeInsets.symmetric(horizontal: screenWidth * 0.03),
       child: Column(
         crossAxisAlignment: CrossAxisAlignment.start,
         children: [
-          const Text(
-            'Description:',
+          Text(
+            'Overview:',
             style: TextStyle(
               color: Colors.white,
-              fontSize: 24,
+              fontSize:
+                  screenWidth > 500 ? screenWidth * 0.04 : screenWidth * 0.05,
+              fontWeight: FontWeight.bold,
+              fontFamily: 'Emad',
+            ),
+          ),
+          SizedBox(height: screenHeight * 0.01),
+          Text(
+            description,
+            style: TextStyle(
+              color: Colors.grey,
+              fontSize:
+                  screenWidth > 500 ? screenWidth * 0.035 : screenWidth * 0.045,
+              fontStyle: FontStyle.italic,
               fontWeight: FontWeight.bold,
             ),
           ),
-          const SizedBox(height: 10),
+          SizedBox(height: screenHeight * 0.02),
           Text(
-            description,
-            style: const TextStyle(
-              color: Colors.grey,
-              fontSize: 16,
-              fontStyle: FontStyle.italic,
+            'Photos of the movie:',
+            style: TextStyle(
+              color: const Color.fromARGB(255, 255, 255, 255),
+              fontSize:
+                  screenWidth > 500 ? screenWidth * 0.035 : screenWidth * 0.045,
               fontWeight: FontWeight.bold,
+              fontFamily: 'Emad',
             ),
           ),
         ],
