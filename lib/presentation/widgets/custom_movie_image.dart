0eb7b001959d730cce0ba7179c591f89f6c7ba6d
zeyadabdelsattar <zeyadabdelsattar@gmail.com>
2024-09-23 00:18:01 +0300

    Add some shadows and make it responsive

--- lib/presentation/widgets/custom_movie_image.dart
+++ lib/presentation/widgets/custom_movie_image.dart
@@ -10,21 +10,30 @@ class CustomMovieImage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     final screenHeight = MediaQuery.of(context).size.height;
+    final screenWidth = MediaQuery.of(context).size.width;
 
     return Stack(
       children: [
-        SizedBox(
-          height: screenHeight * 0.35,
+        Container(
+          height: screenHeight * 0.45,
           width: double.infinity,
+          decoration: BoxDecoration(
+            borderRadius: BorderRadius.circular(16.0),
+            boxShadow: [
+              BoxShadow(
+                color: Colors.black.withOpacity(0.15),
+                blurRadius: 10.0,
+                offset: const Offset(0, 5),
+              ),
+            ],
+          ),
           child: ClipRRect(
-            borderRadius: BorderRadius.circular(
-                8.0), // Optional: if you want rounded corners
+            borderRadius: BorderRadius.circular(16.0),
             child: CachedNetworkImage(
-              height: screenHeight * 0.35,
-              width: double.infinity,
+              fadeOutCurve: Curves.easeIn,
+              fadeInCurve: Curves.easeInOutCirc,
               imageUrl: img,
-              fit: BoxFit
-                  .cover, // Use BoxFit.cover to ensure the image covers the entire box
+              fit: BoxFit.fill,
               placeholder: (context, url) => Center(
                 child: CircularProgressIndicator(
                   color: kSecondaryColor,
@@ -34,14 +43,21 @@ class CustomMovieImage extends StatelessWidget {
             ),
           ),
         ),
+        // Positioned back button
         Positioned(
-          top: 10,
-          left: 10,
-          child: IconButton(
-            icon: const Icon(Icons.arrow_back, color: Colors.white),
-            onPressed: () {
-              Navigator.of(context).pop();
-            },
+          top: 15,
+          left: 15,
+          child: Container(
+            decoration: BoxDecoration(
+              shape: BoxShape.circle,
+              color: Colors.black.withOpacity(0.5),
+            ),
+            child: IconButton(
+              icon: const Icon(Icons.arrow_back, color: Colors.white),
+              onPressed: () {
+                Navigator.of(context).pop();
+              },
+            ),
           ),
         ),
       ],
