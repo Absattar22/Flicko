f8d4a2ffb77d6bb9c156736f8481d513bbf99c27
zeyadabdelsattar <zeyadabdelsattar@gmail.com>
2024-10-12 01:04:46 +0300

    modify the model to add id for each category

--- lib/models/categories_model.dart
+++ lib/models/categories_model.dart
@@ -1,9 +1,11 @@
 class CategoriesModel {
   final String title;
   final String imgUrl;
+  final int id;
 
   CategoriesModel({
     required this.title,
     required this.imgUrl,
+    required this.id,
   });
 }
