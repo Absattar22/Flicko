9bcbe54a44cf0cf91062267f1a3b9084cf55a3dd
zeyadabdelsattar <zeyadabdelsattar@gmail.com>
2024-10-22 04:15:45 +0300

    Add watchlist cubit

--- lib/presentation/views/movie_details_view.dart
+++ lib/presentation/views/movie_details_view.dart
@@ -1,10 +1,12 @@
+import 'package:flicko/cubit/AddToWatchListCubit/add_to_watch_list_cubit_cubit.dart';
+import 'package:flicko/data/api.dart';
 import 'package:flicko/presentation/widgets/custom_movie_recommendation.dart';
+import 'package:flicko/presentation/widgets/custom_movie_title.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:flicko/cubit/movieDetailsCubit/movie_details_cubit.dart';
 import 'package:flicko/constants.dart';
 import 'package:flicko/presentation/widgets/custom_movie_image.dart';
-import 'package:flicko/presentation/widgets/custom_movie_title.dart';
 import 'package:flicko/presentation/widgets/custom_movie_description.dart';
 import 'package:flicko/presentation/widgets/movie_attribute_builder.dart';
 import 'package:flicko/presentation/widgets/custom_movie_photos.dart';
@@ -26,98 +28,103 @@ class _MovieDetailsViewState extends State<MovieDetailsView> {
     final screenHeight = MediaQuery.of(context).size.height;
     final screenWidth = MediaQuery.of(context).size.width;
 
-    return Scaffold(
-      body: BlocBuilder<MovieDetailsCubit, MovieDetailsState>(
-        builder: (context, state) {
-          if (state is MovieDetailsLoading) {
-            return Center(
-              child: CircularProgressIndicator(
-                color: kSecondaryColor,
-              ),
-            );
-          } else if (state is MovieDetailsLoaded) {
-            final movie = state.movie;
-            final backdrops = state.backdrop;
-            final similarMovies = state.similarMovies;
+    return BlocProvider(
+      create: (context) => AddToWatchListCubit(apiService: ApiService()),
+      child: Scaffold(
+        body: BlocBuilder<MovieDetailsCubit, MovieDetailsState>(
+          builder: (context, state) {
+            if (state is MovieDetailsLoading) {
+              return Center(
+                child: CircularProgressIndicator(
+                  color: kSecondaryColor,
+                ),
+              );
+            } else if (state is MovieDetailsLoaded) {
+              final movie = state.movie;
+              final backdrops = state.backdrop;
+              final similarMovies = state.similarMovies;
 
-            return Scaffold(
-              backgroundColor: kPrimaryColor,
-              body: SingleChildScrollView(
-                child: Column(
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  mainAxisAlignment: MainAxisAlignment.start,
-                  children: [
-                    CustomMovieImage(
-                      img: movie.fullImageUrl(),
-                    ),
-                    CustomMovieTitle(
-                      movieTitle: movie.title,
-                      rating: double.parse(movie.rating.toStringAsFixed(1)),
-                      genre1: movie.genres.isNotEmpty ? movie.genres[0] : '',
-                      genre2: movie.genres.length > 1 ? movie.genres[1] : '',
-                      genre3:
-                          movie.genres.length > 2 && movie.genres[2].isNotEmpty
-                              ? movie.genres[2]
-                              : 'Unknown',
-                    ),
-                    MovieAttributeBuilder(
-                      language: movie.language,
-                      date: movie.releaseDate,
-                      duration: movie.duration.toString(),
-                      rating: movie.rating.toStringAsFixed(1),
-                    ),
-                    CustomMovieDescription(
-                      description: movie.overview,
-                    ),
-                    SizedBox(height: screenHeight * 0.01),
-                    CustomMoviePhotos(
-                      imageUrls: backdrops.length > 6
-                          ? backdrops
-                              .skip(24)
-                              .take(12)
-                              .map((backdrop) => backdrop.fullImageUrl())
-                              .toList()
-                          : backdrops
-                              .take(6)
-                              .map((backdrop) => backdrop.fullImageUrl())
-                              .toList(),
-                    ),
-                    SizedBox(height: screenHeight * 0.01),
-                    Padding(
-                      padding:
-                          EdgeInsets.symmetric(horizontal: screenWidth * 0.03),
-                      child: Text(
-                        'You May Also Like:',
-                        style: TextStyle(
-                          color: Colors.white,
-                          fontSize: screenWidth > 500
-                              ? screenWidth * 0.04
-                              : screenWidth * 0.05,
-                          fontWeight: FontWeight.bold,
-                          fontFamily: 'Emad',
+              return Scaffold(
+                backgroundColor: kPrimaryColor,
+                body: SingleChildScrollView(
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    mainAxisAlignment: MainAxisAlignment.start,
+                    children: [
+                      CustomMovieImage(
+                        img: movie.fullImageUrl(),
+                      ),
+                      CustomMovieTitle(
+                        movieId: movie.id,
+                        movieTitle: movie.title,
+                        rating: double.parse(movie.rating.toStringAsFixed(1)),
+                        genre1: movie.genres.isNotEmpty ? movie.genres[0] : '',
+                        genre2: movie.genres.length > 1 ? movie.genres[1] : '',
+                        genre3: movie.genres.length > 2 &&
+                                movie.genres[2].isNotEmpty
+                            ? movie.genres[2]
+                            : 'Unknown',
+                      ),
+                      MovieAttributeBuilder(
+                        language: movie.language,
+                        date: movie.releaseDate,
+                        duration: movie.duration.toString(),
+                        rating: movie.rating.toStringAsFixed(1),
+                      ),
+                      CustomMovieDescription(
+                        description: movie.overview,
+                      ),
+                      SizedBox(height: screenHeight * 0.01),
+                      CustomMoviePhotos(
+                        imageUrls: backdrops.length > 6
+                            ? backdrops
+                                .skip(24)
+                                .take(12)
+                                .map((backdrop) => backdrop.fullImageUrl())
+                                .toList()
+                            : backdrops
+                                .take(6)
+                                .map((backdrop) => backdrop.fullImageUrl())
+                                .toList(),
+                      ),
+                      SizedBox(height: screenHeight * 0.01),
+                      Padding(
+                        padding: EdgeInsets.symmetric(
+                            horizontal: screenWidth * 0.03),
+                        child: Text(
+                          'You May Also Like:',
+                          style: TextStyle(
+                            color: Colors.white,
+                            fontSize: screenWidth > 500
+                                ? screenWidth * 0.04
+                                : screenWidth * 0.05,
+                            fontWeight: FontWeight.bold,
+                            fontFamily: 'Emad',
+                          ),
                         ),
                       ),
-                    ),
-                    CustomMovieRecommendation(recommendedMovies: similarMovies)
-                  ],
+                      CustomMovieRecommendation(
+                          recommendedMovies: similarMovies)
+                    ],
+                  ),
                 ),
-              ),
-            );
-          } else if (state is MovieDetailsError) {
-            return Center(
+              );
+            } else if (state is MovieDetailsError) {
+              return Center(
+                child: Text(
+                  'Error: ${state.message}',
+                  style: const TextStyle(color: Colors.red, fontSize: 16),
+                ),
+              );
+            }
+            return const Center(
               child: Text(
-                'Error: ${state.message}',
-                style: const TextStyle(color: Colors.red, fontSize: 16),
+                'No details available',
+                style: TextStyle(color: Colors.white, fontSize: 16),
               ),
             );
-          }
-          return const Center(
-            child: Text(
-              'No details available',
-              style: TextStyle(color: Colors.white, fontSize: 16),
-            ),
-          );
-        },
+          },
+        ),
       ),
     );
   }
