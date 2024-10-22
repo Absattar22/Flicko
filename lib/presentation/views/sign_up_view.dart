2e2a754f1a8037c9e479543769207a65ed87b05d
zeyadabdelsattar <zeyadabdelsattar@gmail.com>
2024-10-22 04:12:14 +0300

    Add the indicator for signin with google

--- lib/presentation/views/sign_up_view.dart
+++ lib/presentation/views/sign_up_view.dart
@@ -9,7 +9,6 @@ import '../widgets/other_sign_in_options.dart';
 import '../widgets/sign_in_with_google.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_svg/svg.dart';
-import 'package:modal_progress_hud_nsn/modal_progress_hud_nsn.dart';
 
 class SignUpView extends StatefulWidget {
   static String id = 'signUpView';
@@ -21,7 +20,8 @@ class SignUpView extends StatefulWidget {
 }
 
 class _SignUpViewState extends State<SignUpView> {
-  bool isLoading = false;
+  bool isLoading = false; 
+  bool isGoogleLoading = false; // دا ل جوجل
   final formKey = GlobalKey<FormState>();
   String? email;
   String? password;
@@ -32,191 +32,187 @@ class _SignUpViewState extends State<SignUpView> {
     final screenHeight = MediaQuery.of(context).size.height;
     final screenWidth = MediaQuery.of(context).size.width;
 
-    return ModalProgressHUD(
-      inAsyncCall: isLoading,
-      child: Scaffold(
-        backgroundColor: kPrimaryColor,
-        body: SafeArea(
-          child: Form(
-            key: formKey,
-            child: SingleChildScrollView(
-              child: Padding(
-                padding: EdgeInsets.symmetric(horizontal: screenWidth * 0.05),
-                child: Column(
-                  children: [
-                    SizedBox(height: screenHeight * 0.001),
-                    SvgPicture.asset(
-                      'assets/images/sign_up.svg',
-                      height: screenHeight * 0.22,
+    return Scaffold(
+      backgroundColor: kPrimaryColor,
+      body: SafeArea(
+        child: Form(
+          key: formKey,
+          child: SingleChildScrollView(
+            child: Padding(
+              padding: EdgeInsets.symmetric(horizontal: screenWidth * 0.05),
+              child: Column(
+                children: [
+                  SizedBox(height: screenHeight * 0.001),
+                  SvgPicture.asset(
+                    'assets/images/sign_up.svg',
+                    height: screenHeight * 0.22,
+                  ),
+                  SizedBox(height: screenHeight * 0.015),
+                  Text(
+                    'Welcome To Flicko',
+                    style: TextStyle(
+                      color: kSecondaryColor,
+                      fontSize: screenHeight * 0.03,
+                      fontFamily: 'Emad',
+                      fontWeight: FontWeight.bold,
                     ),
-                    SizedBox(height: screenHeight * 0.015),
-                    Text(
-                      'Welcome To Flicko',
-                      style: TextStyle(
-                        color: kSecondaryColor,
-                        fontSize: screenHeight * 0.03,
-                        fontFamily: 'figtree',
-                        fontWeight: FontWeight.bold,
-                      ),
+                  ),
+                  Text(
+                    'Sign Up To Get Started',
+                    style: TextStyle(
+                      color: Colors.white,
+                      fontSize: screenHeight * 0.022,
+                      fontFamily: 'Emad',
+                      fontWeight: FontWeight.bold,
                     ),
-                    Text(
-                      'Sign Up To Get Started',
-                      style: TextStyle(
-                        color: Colors.white,
-                        fontSize: screenHeight * 0.022,
-                        fontFamily: 'figtree',
-                        fontWeight: FontWeight.bold,
-                      ),
-                    ),
-                    SizedBox(height: screenHeight * 0.015),
-                    CustomFormTextField(
-                      onChanged: (value) {
-                        email = value;
-                      },
-                      hint: 'Enter Your Email',
-                      obscureText: false,
-                      text: 'Email',
-                      isSignup: true,
-                    ),
-                    SizedBox(height: screenHeight * 0.01),
-                    CustomFormTextField(
-                      onChanged: (value) {
-                        password = value;
-                      },
-                      hint: 'Enter Your Password',
-                      obscureText: isObscured,
-                      text: 'Password',
-                      isSignup: true,
-                    ),
-                    SizedBox(height: screenHeight * 0.04),
-                    CustomButton(
-                      text: 'Sign Up',
-                      color1: kSecondaryColor,
-                      color2: kSecondaryColor,
-                      txtColor: Colors.white,
-                      borderColor: Colors.black,
-                      onTap: () async {
-                        if (formKey.currentState!.validate()) {
-                          setState(() {
-                            isLoading = true;
-                          });
-                          try {
-                            await SignUpNewUser();
-                            Navigator.of(context).pushNamedAndRemoveUntil(
-                                MovieView.id, (route) => false);
-                          } on FirebaseAuthException catch (e) {
-                            if (e.code == 'weak-password') {
-                              ShowSnackBar(
-                                  context,
-                                  'The password provided is too weak.',
-                                  Colors.redAccent,
-                                  Icons.error);
-                            } else if (e.code == 'email-already-in-use') {
-                              ShowSnackBar(
-                                  context,
-                                  'The account already exists.',
-                                  Colors.blueAccent,
-                                  Icons.email);
-                            } else {
-                              ShowSnackBar(
-                                  context,
-                                  'An error occurred. Please try again.',
-                                  Colors.red,
-                                  Icons.error);
-                            }
-                          } finally {
-                            setState(() {
-                              isLoading = false;
-                            });
-                          }
-                        }
-                      },
-                    ),
-                    SizedBox(height: screenHeight * 0.015),
-                    Row(
-                      mainAxisAlignment: MainAxisAlignment.center,
-                      children: [
-                        Expanded(
-                          child: Container(
-                            margin: EdgeInsets.symmetric(
-                                horizontal: screenWidth * 0.03),
-                            height: 1.0,
-                            color: Colors.white,
-                          ),
-                        ),
-                        Text(
-                          'OR',
-                          style: TextStyle(
-                            color: Colors.white,
-                            fontSize: screenHeight * 0.018,
-                          ),
-                        ),
-                        Expanded(
-                          child: Container(
-                            margin: EdgeInsets.symmetric(
-                                horizontal: screenWidth * 0.03),
-                            height: 1.0,
-                            color: Colors.white,
-                          ),
-                        ),
-                      ],
-                    ),
-                    SizedBox(height: screenHeight * 0.015),
-                    OtherSignInOptions(
-                      txtColor: const Color.fromARGB(255, 30, 30, 30),
-                      borderColor: const Color.fromARGB(255, 30, 30, 30),
-                      text: 'Sign in with Google',
-                      img: 'assets/images/google.png',
-                      onTap: () async {
+                  ),
+                  SizedBox(height: screenHeight * 0.015),
+                  CustomFormTextField(
+                    onChanged: (value) {
+                      email = value;
+                    },
+                    hint: 'Enter Your Email',
+                    obscureText: false,
+                    text: 'Email',
+                    isSignup: true,
+                  ),
+                  SizedBox(height: screenHeight * 0.01),
+                  CustomFormTextField(
+                    onChanged: (value) {
+                      password = value;
+                    },
+                    hint: 'Enter Your Password',
+                    obscureText: isObscured,
+                    text: 'Password',
+                    isSignup: true,
+                  ),
+                  SizedBox(height: screenHeight * 0.025),
+                  CustomButton(
+                    text: 'Sign Up',
+                    color1: kSecondaryColor,
+                    color2: kSecondaryColor,
+                    txtColor: Colors.white,
+                    borderColor: Colors.black,
+                    isLoading: isLoading, // Pass the loading state
+                    onTap: () async {
+                      if (formKey.currentState!.validate()) {
                         setState(() {
-                          isLoading = true;
+                          isLoading = true; // Start loading
                         });
                         try {
-                          await signInWithGoogle();
+                          await SignUpNewUser();
                           Navigator.of(context).pushNamedAndRemoveUntil(
                               MovieView.id, (route) => false);
-                        } catch (e) {
-                          ShowSnackBar(context, 'Sign in failed.', Colors.red,
-                              Icons.error);
+                        } on FirebaseAuthException catch (e) {
+                          if (e.code == 'weak-password') {
+                            ShowSnackBar(
+                                context,
+                                'The password provided is too weak.',
+                                Colors.redAccent,
+                                Icons.error);
+                          } else if (e.code == 'email-already-in-use') {
+                            ShowSnackBar(context, 'The account already exists.',
+                                Colors.blueAccent, Icons.email);
+                          } else {
+                            ShowSnackBar(
+                                context,
+                                'An error occurred. Please try again.',
+                                Colors.red,
+                                Icons.error);
+                          }
                         } finally {
                           setState(() {
-                            isLoading = false;
+                            isLoading = false; // Stop loading
                           });
                         }
-                      },
-                    ),
-                    SizedBox(height: screenHeight * 0.002),
-                    Row(
-                      mainAxisAlignment: MainAxisAlignment.center,
-                      children: [
-                        Text(
-                          'Already Have An Account?',
+                      }
+                    },
+                  ),
+                  SizedBox(height: screenHeight * 0.015),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      Expanded(
+                        child: Container(
+                          margin: EdgeInsets.symmetric(
+                              horizontal: screenWidth * 0.03),
+                          height: 1.0,
+                          color: Colors.white,
+                        ),
+                      ),
+                      Text(
+                        'OR',
+                        style: TextStyle(
+                          color: Colors.white,
+                          fontSize: screenHeight * 0.018,
+                        ),
+                      ),
+                      Expanded(
+                        child: Container(
+                          margin: EdgeInsets.symmetric(
+                              horizontal: screenWidth * 0.03),
+                          height: 1.0,
+                          color: Colors.white,
+                        ),
+                      ),
+                    ],
+                  ),
+                  SizedBox(height: screenHeight * 0.015),
+                  OtherSignInOptions(
+                    txtColor: const Color.fromARGB(255, 30, 30, 30),
+                    borderColor: const Color.fromARGB(255, 30, 30, 30),
+                    text: 'Sign Up With Google',
+                    img: 'assets/images/google.png',
+                    isGoogleLoading: isGoogleLoading, // Pass loading state for Google sign in
+                    onTap: () async {
+                      setState(() {
+                        isGoogleLoading = true; // Start loading
+                      });
+                      try {
+                        await signInWithGoogle();
+                        Navigator.of(context).pushNamedAndRemoveUntil(
+                            MovieView.id, (route) => false);
+                      } catch (e) {
+                        ShowSnackBar(context, 'Sign in failed.', Colors.red,
+                            Icons.error);
+                      } finally {
+                        setState(() {
+                          isGoogleLoading = false; // Stop loading
+                        });
+                      }
+                    },
+                  ),
+                  SizedBox(height: screenHeight * 0.002),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      Text(
+                        'Already Have An Account?',
+                        style: TextStyle(
+                          color: const Color.fromARGB(255, 173, 169, 169),
+                          fontSize: screenHeight * 0.018,
+                        ),
+                      ),
+                      TextButton(
+                        onPressed: () {
+                          Navigator.of(context).pushNamedAndRemoveUntil(
+                              SignInView.id, (route) => false);
+                        },
+                        child: Text(
+                          'Sign In',
                           style: TextStyle(
-                            color: const Color.fromARGB(255, 173, 169, 169),
+                            color: Colors.white,
                             fontSize: screenHeight * 0.018,
+                            decoration: TextDecoration.underline,
+                            decorationColor: Colors.white,
+                            fontWeight: FontWeight.bold,
                           ),
                         ),
-                        TextButton(
-                          onPressed: () {
-                            Navigator.of(context).pushNamedAndRemoveUntil(
-                                SignInView.id, (route) => false);
-                          },
-                          child: Text(
-                            'Sign In',
-                            style: TextStyle(
-                              color: Colors.white,
-                              fontSize: screenHeight * 0.018,
-                              decoration: TextDecoration.underline,
-                              decorationColor: Colors.white,
-                              fontWeight: FontWeight.bold,
-                            ),
-                          ),
-                        )
-                      ],
-                    ),
-                    SizedBox(height: screenHeight * 0.02),
-                  ],
-                ),
+                      )
+                    ],
+                  ),
+                  SizedBox(height: screenHeight * 0.02),
+                ],
               ),
             ),
           ),
